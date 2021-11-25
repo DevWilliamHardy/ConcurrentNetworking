3dc17014275b46544ffbbb262277f39e96cb3731
William Hardy <53836081+DevWilliamHardy@users.noreply.github.com>
2021-11-25 09:04:15 +0000

    work

---
+++ Concurrency/Concurrency/ThreadApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Threading;
+
+namespace Concurrency
+{
+    class ThreadApp
+    {
+        public static void Main()
+        {
+            Threadinfo info1 = new Threadinfo(100, 100);
+            Threadinfo info2 = new Threadinfo(0, 200);
+
+            Thread thread1 = new Thread(new ThreadStart(info1.DoWork));
+            Thread thread2 = new Thread(new ThreadStart(info2.DoWork));
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
---
+++ Concurrency/Concurrency/ThreadInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    
+    public class Threadinfo
+    {
+        private int num;
+        private int pause;
+        
+        public Threadinfo(int num, int pause)
+        { this.num = num; this.pause = pause; }
+
+        public void DoWork()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(pause);
+                Console.WriteLine(num + 1);
+            }
+        }
+    }
+}
