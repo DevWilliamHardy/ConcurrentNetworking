60aeee213439a6a3e698e5a5e547019da2742cd6
William Hardy <53836081+DevWilliamHardy@users.noreply.github.com>
2021-11-26 08:21:34 +0000

    Task1

--- Concurrency/Concurrency/ThreadApp.cs
+++ Concurrency/Concurrency/ThreadApp.cs
@@ -1,25 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Threading;
 
-namespace Concurrency
+public class ThreadingStuff
 {
-    class ThreadApp
+    public static void Sort(int[] SortNumbers, int count)
     {
-        public static void Main()
+
+        int temp, smallest;
+        for(int i = 0; i < 10; i++)
         {
-            Threadinfo info1 = new Threadinfo(101, 100);
-            Threadinfo info2 = new Threadinfo(100, 100);
-
-            Thread countDown = new Thread(new ThreadStart(info2.Decrease));
-            Thread countUp = new Thread(new ThreadStart(info1.Increase));
-
-            countDown.Start();
-            countUp.Start();
+            smallest = i;
+            for(int j = i + 1; j < 10; j++)
+            {
+                if (SortNumbers[j] < SortNumbers[smallest])
+                {
+                    smallest = j;
+                }
+                temp = SortNumbers[smallest];
+                SortNumbers[smallest] = SortNumbers[i];
+                SortNumbers[i] = temp;
+            }
         }
+        Console.WriteLine("Sorted: " + count);
+    }
+}
+public class SortArrays
+{
+    static void Generate(int[] NewNumbers)
+    {
+        System.Random random = new System.Random();
+
+        for(int i = 0; i < 10; i++)
+        {
+            NewNumbers[i] = random.Next(50);
+        }
+        
+    }
+    static void Toscreen(int[] NumsToPrint)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+           Console.Write(NumsToPrint[i] + " ");
+        }
+        Console.WriteLine();
+    }
+    static void Main(string[] args)
+    {
+        int[] Numbers1 = new int[10];
+        int[] Numbers2 = new int[10];
+        int[] Numbers3 = new int[10];
+
+        Generate(Numbers1);
+        Generate(Numbers2);
+        Generate(Numbers3);
+
+        Console.WriteLine("Initial Lists Are: ");
+        Console.WriteLine();
+
+        Toscreen(Numbers1);
+        Toscreen(Numbers2);
+        Toscreen(Numbers3);
+
+        Thread Thread1 = new Thread(new ThreadStart(() => ThreadingStuff.Sort(Numbers1, 1)));
+        Thread Thread2 = new Thread(new ThreadStart(() => ThreadingStuff.Sort(Numbers2, 3)));
+        Thread Thread3 = new Thread(new ThreadStart(() => ThreadingStuff.Sort(Numbers3, 3)));
+
+        Thread1.Start();
+        Thread2.Start();
+        Thread3.Start();
+
+        Console.WriteLine("Sorted Lists Are: ");
+        Console.WriteLine();
+
+        Toscreen(Numbers1);
+        Toscreen(Numbers2);
+        Toscreen(Numbers3);
     }
 }
