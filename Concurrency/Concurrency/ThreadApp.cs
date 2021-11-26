fb6dc2a0b1bb187f78b6803d56e3c21740d5c57a
William Hardy <53836081+DevWilliamHardy@users.noreply.github.com>
2021-11-26 06:24:56 +0000

    Task 2

--- Concurrency/Concurrency/ThreadApp.cs
+++ Concurrency/Concurrency/ThreadApp.cs
@@ -12,14 +12,24 @@ namespace Concurrency
     {
         public static void Main()
         {
-            Threadinfo info1 = new Threadinfo(101, 100);
-            Threadinfo info2 = new Threadinfo(100, 100);
+            Threadinfo info1 = new Threadinfo(1);
+            Threadinfo info2 = new Threadinfo(2);
+            Threadinfo info3 = new Threadinfo(3);
+            Threadinfo info4 = new Threadinfo(4);
+            Threadinfo info5 = new Threadinfo(5);
+            
 
-            Thread countDown = new Thread(new ThreadStart(info2.Decrease));
-            Thread countUp = new Thread(new ThreadStart(info1.Increase));
+            Thread thread1 = new Thread(new ThreadStart(info1.PrintInt));
+            Thread thread2 = new Thread(new ThreadStart(info2.PrintInt));
+            Thread thread3 = new Thread(new ThreadStart(info3.PrintInt));
+            Thread thread4 = new Thread(new ThreadStart(info4.PrintInt));
+            Thread thread5 = new Thread(new ThreadStart(info5.PrintInt));
 
-            countDown.Start();
-            countUp.Start();
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+            thread5.Start();
         }
     }
 }
--- Concurrency/Concurrency/ThreadInfo.cs
+++ Concurrency/Concurrency/ThreadInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Timers;
 
 namespace Threading
 {
@@ -9,25 +10,15 @@ namespace Threading
         private int maxNum;
         private int pause;
         
-        public Threadinfo(int maxNum, int pause)
-        { this.maxNum = maxNum; this.pause = pause; }
+        public Threadinfo(int maxNum)
+        { this.maxNum = maxNum; pause = maxNum * 500; }
 
-        public void Decrease()
+        public void PrintInt()
         {
-            for (int i = maxNum; i > 0; i--)
-            {
-                Thread.Sleep(pause);
-                Console.WriteLine(maxNum -= 1);
-            }
-        }
-        public void Increase()
-        {
-            int x = maxNum;
-            for(int i = 0; i < x; i++)
-            {
-                Thread.Sleep(pause);
-                Console.WriteLine(i);
-            }
+            Console.WriteLine(maxNum);
+            Thread.Sleep(pause);
+            PrintInt();
+
         }
     }
 }
