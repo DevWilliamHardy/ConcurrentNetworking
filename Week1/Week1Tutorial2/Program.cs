c1f4b158c7aa92a7bf39584e1cf68af2f3272d52
William Hardy <53836081+DevWilliamHardy@users.noreply.github.com>
2022-03-26 11:38:27 +0000

    Week1Tutorial2

---
+++ Week1/Week1Tutorial2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input rectangle width: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Input rectangle height: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        double area = x * y;
+
+        double perimeter = 2 * x + 2 * y;
+
+        Console.WriteLine("Area: {0}\nPerimeter: {1}", area, perimeter);
+    }
+}
